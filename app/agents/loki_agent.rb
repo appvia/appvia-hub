1f83b6e7dd4e53ba118adc8307f79367abbb2497
Daniel Whatmuff <danielwhatmuff@gmail.com>
2019-06-12 12:15:01 +0100

    rubocopping and cleaning up

--- app/agents/loki_agent.rb
+++ app/agents/loki_agent.rb
@@ -5,9 +5,22 @@ class LokiAgent
     @grafana_url = grafana_url
   end
 
-  def construct_url(grafana_url, namespace)
-    logging_view_uri = "explore?left=[\"now-6h\",\"now\",\"Loki\",{\"expr\":\"{namespace=" + '\"' + "#{namespace}" + '\"' + "}\"},{\"ui\":[true,true,true,\"none\"]}]"
-    logging_view_url = "#{grafana_url}/" + CGI.escape(logging_view_uri)
-    puts logging_view_url
+  def construct_logging_view_url(namespace)
+    logging_view_uri = 'explore?left=[
+                          "now-6h",
+                          "now",
+                          "Loki",
+                          {
+                            "expr":"{namespace=\"' + namespace + '\"}
+                          },
+                          {
+                            "ui":[true,true,true,"none"]
+                          }
+                        ]'
+    if grafana_url.end_with?('/')
+      @grafana_url + '/' + CGI.escape(logging_view_uri)
+    else
+      @grafana_url + CGI.escape(logging_view_uri)
+    end
   end
 end
