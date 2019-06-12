79b0a44ef1192d117c8ac54e247aad86bc363154
Daniel Whatmuff <danielwhatmuff@gmail.com>
2019-06-12 12:45:52 +0100

    updating delete function

--- app/agents/loki_agent.rb
+++ app/agents/loki_agent.rb
@@ -25,21 +25,6 @@ class LokiAgent
   end
 
   def delete_logging_view(namespace)
-    logging_view_uri = 'explore?left=[
-                          "now-6h",
-                          "now",
-                          "Loki",
-                          {
-                            "expr":"{namespace=\"' + namespace + '\"}
-                          },
-                          {
-                            "ui":[true,true,true,"none"]
-                          }
-                        ]'
-    if grafana_url.end_with?('/')
-      @grafana_url + '/' + CGI.escape(logging_view_uri)
-    else
-      @grafana_url + CGI.escape(logging_view_uri)
-    end
+    true
   end
 end
