557a6afe95515a90c2e5621277eb1680b0cf8ead
Daniel Whatmuff <danielwhatmuff@gmail.com>
2019-06-12 11:46:12 +0100

    linting...

--- app/agents/loki_agent.rb
+++ app/agents/loki_agent.rb
@@ -6,7 +6,7 @@ class LokiAgent
   end
 
   def construct_url(grafana_url, namespace)
-    logging_view_uri="explore?left=[\"now-6h\",\"now\",\"Loki\",{\"expr\":\"{namespace=" + '\"' + "#{namespace}" + '\"' + "}\"},{\"ui\":[true,true,true,\"none\"]}]"
+    logging_view_uri = "explore?left=[\"now-6h\",\"now\",\"Loki\",{\"expr\":\"{namespace=" + '\"' + "#{namespace}" + '\"' + "}\"},{\"ui\":[true,true,true,\"none\"]}]"
     logging_view_url = "#{grafana_url}/" + CGI.escape(logging_view_uri)
     puts logging_view_url
   end
