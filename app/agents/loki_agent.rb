be41bb3d6dce8ee0b04b7cc82acc37dda9d4e5d5
Daniel Whatmuff <danielwhatmuff@gmail.com>
2019-06-13 15:15:29 +0100

    updating to use loki expression as argument

--- app/agents/loki_agent.rb
+++ app/agents/loki_agent.rb
@@ -1,17 +1,16 @@
-require 'cgi'
-
 class LokiAgent
   def initialize(grafana_url:)
     @grafana_url = grafana_url
   end
 
-  def create_logging_dashboard(name)
+  def create_logging_dashboard(name, query_expression)
+    logger.info "Creating logging dashboard #{name}"
     logging_view_uri = 'explore?left=[
                           "now-6h",
                           "now",
                           "Loki",
                           {
-                            "expr":"{namespace=\"' + namespace + '\"}
+                            "expr":"' + query_expression + '"
                           },
                           {
                             "ui":[true,true,true,"none"]
@@ -25,6 +24,7 @@ class LokiAgent
   end
 
   def delete_logging_dashboard(name)
+    logger.info "Deleting logging dashboard #{name}"
     true
   end
 end
