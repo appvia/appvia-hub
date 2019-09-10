832770fcead91e68f88287852ffe7c5843776398
David Thompson <davethompson21@gmail.com>
2019-09-10 09:51:18 +0100

    Fixing typo: overriden -> overridden

--- spec/integration/resources/code_repos/git_hub_code_repos_overridden_config_integration_spec.rb
+++ spec/integration/resources/code_repos/git_hub_code_repos_overridden_config_integration_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Code Repo - GitHub - with overriden config option' do
+RSpec.describe 'Code Repo - GitHub - with overridden config option' do
   let! :provisioning_service do
     ResourceProvisioningService.new
   end
@@ -72,7 +72,7 @@ RSpec.describe 'Code Repo - GitHub - with overriden config option' do
   end
 
   describe 'request create' do
-    it 'agent should receive the overriden config option' do
+    it 'agent should receive the overridden config option' do
       expect(agent).to receive(:create_repository)
         .with(resource.name, team_id: 1000, auto_init: false)
         .and_return(agent_create_response)
