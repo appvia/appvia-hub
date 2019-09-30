47569c3b634cb71321b3f328e88e9defccbb4a13
Jits <dev@jits.co.uk>
2019-09-30 09:54:40 +0100

    Revamped 'Access' page
    This now takes into account the project robot credentials for the user, for Quay and Kubernetes integrations.

--- app/controllers/me/access_controller.rb
+++ app/controllers/me/access_controller.rb
@@ -14,6 +14,14 @@ module Me
         .for_user(current_user)
         .group_by(&:provider_id)
 
+      project_ids = current_user
+        .teams
+        .map(&:project_ids)
+        .flatten
+      project_robot_credentials_by_integration = ProjectRobotCredentialsService
+        .for_projects(project_ids)
+        .group_by(&:integration_id)
+
       @groups = ResourceTypesService.all.map do |rt|
         integrations = rt[:providers].reduce([]) do |acc, p|
           acc + Array(integrations_by_provider[p])
@@ -22,12 +30,15 @@ module Me
         entries = integrations.map do |i|
           {
             integration: i,
-            identity: identities_by_integration[i.id]
+            identity: identities_by_integration[i.id],
+            project_robot_credentials: Array(project_robot_credentials_by_integration[i.id])
           }
         end
 
         rt.merge entries: entries
       end
+
+      @unmask = params.key? 'unmask'
     end
   end
 end
