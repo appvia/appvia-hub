da1555cc41f648ac95e9ca20bb0c2ec9f16b2490
Daniel Whatmuff <danielwhatmuff@gmail.com>
2019-10-17 13:52:39 +0100

    updated grafana test

--- spec/integration/teams/grafana_teams_integration_spec.rb
+++ spec/integration/teams/grafana_teams_integration_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Grafana teams end-to-end' do
   include_context 'time helpers'
 
-  let(:agent_class) { GrafanaAgent }
+  let(:provider_id) { 'grafana' }
+  let(:grafana_agent_class) { GrafanaAgent }
+  let(:kubernetes_agent_class) { KubernetesAgent }
 
   let!(:user) { create :user }
   let!(:other_user) { create :user }
@@ -22,28 +24,24 @@ RSpec.describe 'Grafana teams end-to-end' do
 
   context 'with no integrations yet' do
     before do
-      expect(agent_class).to receive(:new).never
+      expect(grafana_agent_class).to receive(:new).never
     end
 
-    it 'doesn\'t synchronise grafana users' do
+    it 'doesn\'t perform any synchronisation of grafana users' do
       team1, success = TeamsService.create(
         {
-          name: 'Team 1',
-          slug: 'team-1',
-          description: 'This is Team numero uno'
+          name: 'Grafana Team',
+          slug: 'grafana-team',
+          description: 'This is a grafana team'
         },
         user
       )
       expect(success).to be true
-      expect(team1.slug).to eq 'team-1'
-
-      # `user` should've been made an admin of the team automatically.
-      expect(TeamMembershipsService.user_an_admin_of_team?(team1.id, user)).to be true
-      expect(TeamMembership.exists?(team: team1, user: user, role: 'admin')).to be true
+      expect(team1.slug).to eq 'grafana-team'
 
       expect(Sidekiq::Worker.jobs.size).to eq 2
 
-      expect_any_instance_of(agent_class).to receive(:sync_team).never
+      expect_any_instance_of(grafana_agent_class).to receive(:sync_team).never
 
       process_jobs
 
@@ -53,89 +51,107 @@ RSpec.describe 'Grafana teams end-to-end' do
 
       expect(Sidekiq::Worker.jobs.size).to eq 1
 
-      expect_any_instance_of(agent_class).to receive(:sync_team).never
+      expect_any_instance_of(grafana_agent_class).to receive(:sync_team).never
 
       process_jobs
     end
   end
 
   context 'with integrations' do
-    let :integration_config do
+
+    let :kubernetes_integration_config do
       {
-        'url' => 'http://my-grafana.com',
-        'api_key' => 'secret api key thing',
-        'ca_cert' => 'some certificate',
-        'template_url' => 'https://s3.amazonaws.com/some-template.json',
-        'admin_username' => 'mrs admin',
-        'admin_password' => 'very secret password'
+        'cluster_name' => 'Our Kube Cluster',
+        'api_url' => 'url-to-kube-api',
+        'ca_cert' => 'kube CA cert',
+        'token' => 'kube API token'
       }
     end
 
-    let :agent_initializer_params do
+    let :kubernetes_agent_initializer_params do
+      {
+        agent_base_url: Rails.configuration.agents.kubernetes.base_url,
+        agent_token: Rails.configuration.agents.kubernetes.token,
+        kube_api_url: kubernetes_integration_config['api_url'],
+        kube_ca_cert: kubernetes_integration_config['ca_cert'],
+        kube_token: kubernetes_integration_config['token']
+      }
+    end
+    let(:kubernetes_agent) { instance_double(kubernetes_agent_class) }
+
+    let :grafana_integration_config do
+      {
+        'url' => 'http://grafana',
+        'api_key' => 'this is an API key, no really',
+        'ca_cert' => 'ca cert base64 encoded',
+        'template_url' => 'http://url.to.my.template/foo',
+        'admin_username' => 'admin',
+        'admin_password' => 'this is an admin password, I promise'
+      }
+    end
+
+    let(:grafana_agent_class) { GrafanaAgent }
+    let :grafana_agent_initializer_params do
       {
         agent_base_url: Rails.configuration.agents.grafana.base_url,
         agent_token: Rails.configuration.agents.grafana.token,
-        url: integration_config['url'],
-        api_key: integration_config['api_key'],
-        ca_cert: integration_config['ca_cert'],
-        admin_username: integration_config['admin_username'],
-        admin_password: integration_config['admin_password']
+        url: grafana_integration_config['url'],
+        api_key: grafana_integration_config['api_key'],
+        ca_cert: grafana_integration_config['ca_cert'],
+        admin_username: grafana_integration_config['admin_username'],
+        admin_password: grafana_integration_config['admin_password']
       }
     end
-    let(:agent) { instance_double(agent_class) }
+    let(:agent) { instance_double(grafana_agent_class) }
 
-    before do
-      expect(agent_class).to receive(:new)
-        .with(**agent_initializer_params)
-        .and_return(agent)
-        .at_least(:once)
-    end
-
-    it 'syncs users up to grafana' do
-      # Start by creating an integration, without any allocations, so open to all
-      # teams.
+    it 'synchronises users with grafana' do
+      # Create a kubernetes and grafana integration
 
       expect(Integration.count).to be 0
 
-      integration, success = Admin::IntegrationsService.create(
-        name: 'Grafana Integration',
-        provider_id: 'grafana',
-        config: integration_config
+      kubernetes_integration, kubernetes_integration_success = Admin::IntegrationsService.create(
+        name: 'Kubernetes Integration',
+        provider_id: 'kubernetes',
+        config: kubernetes_integration_config
       )
 
-      expect(success).to be true
+      expect(kubernetes_integration_success).to be true
       expect(Integration.count).to be 1
-      expect(integration.grafana?).to be true
+      expect(kubernetes_integration.kubernetes?).to be true
 
       expect(Sidekiq::Worker.jobs.size).to eq 1
 
-      process_jobs
+      grafana_integration, grafana_success = Admin::IntegrationsService.create(
+        name: 'Grafana Integration',
+        provider_id: 'grafana',
+        config: grafana_integration_config,
+        parent_ids: [kubernetes_integration.id]
+      )
 
-      # Create a team and check that it syncs users to Grafana.
+      expect(grafana_success).to be true
+      expect(Integration.count).to be 2
+      expect(grafana_integration.grafana?).to be true
 
-      reload_all integration
+      expect(Sidekiq::Worker.jobs.size).to eq 2
 
       team1, success = TeamsService.create(
         {
-          name: 'Team 1',
-          slug: 'team-1',
-          description: 'This is Team numero uno'
+          name: 'Grafana Team',
+          slug: 'grafana-team',
+          description: 'This is a grafana team'
         },
         user
       )
       expect(success).to be true
-      expect(team1.slug).to eq 'team-1'
+      expect(team1.slug).to eq 'grafana-team'
 
-      # `user` should've been made an admin of the team automatically.
-      expect(TeamMembershipsService.user_an_admin_of_team?(team1.id, user)).to be true
-      expect(TeamMembership.exists?(team: team1, user: user, role: 'admin')).to be true
+      expect(Sidekiq::Worker.jobs.size).to eq 4
 
-      expect(Sidekiq::Worker.jobs.size).to eq 2
-
-      expect(agent).to receive(:sync_team)
-        .with(team.memberships)
+      expect_any_instance_of(grafana_agent_class).to receive(:sync_team)
 
       process_jobs
+
+      reload_all team1
     end
   end
 end
