1df36b7e11c0d8c39f182ac77401a37c9049fb78
David Thompson <davethompson21@gmail.com>
2019-10-22 09:19:17 +0100

    Adding quay security checks
    * upgrading the agent to v0.0.7
    * using the lazy loading the same as the other checks

--- app/services/resource_checks_service.rb
+++ app/services/resource_checks_service.rb
@@ -5,6 +5,14 @@ class ResourceChecksService
     'failure' => 'danger'
   }.freeze
 
+  QUAY_STATUS_TO_COLOUR = {
+    'High' => 'danger',
+    'Medium' => 'warning',
+    'Low' => 'info',
+    'Negligible' => 'secondary',
+    'Unknown' => 'secondary'
+  }.freeze
+
   def get_checks(resource)
     config = IntegrationOverridesService.new.effective_config_for resource.integration, resource.project
     agent = AgentsService.get resource.integration.provider_id, config
@@ -14,6 +22,8 @@ class ResourceChecksService
       git_hub_checks resource, agent
     when 'kubernetes'
       kubernetes_checks resource, agent
+    when 'quay'
+      quay_checks resource, agent
     end
   end
 
@@ -69,5 +79,50 @@ class ResourceChecksService
       }
     end
     response
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def quay_checks(resource, agent)
+    response = []
+    status = agent.get_repo_status(resource.name)
+  rescue StandardError => e
+    Rails.logger.warn "Error getting security scan from Quay: #{e}"
+    response << {
+      colour: 'secondary',
+      text: 'Security scan unavailable right now.',
+      status: 'Timeout',
+      url: false
+    }
+  else
+    items = status[:items]
+    items.each do |i|
+      image_tag = i[:name]
+      i[:spec][:features].each do |f|
+        cves = []
+        severities = []
+        f[:vulnerabilities].each do |v|
+          cves << v[:name]
+          severities << v[:severity]
+        end
+        highest_severity = quay_get_highest_severity severities
+        digest = i[:spec][:tag][:digest]
+        next if cves.empty?
+
+        response << {
+          colour: QUAY_STATUS_TO_COLOUR[highest_severity],
+          text: image_tag + ' ' + cves.join(' '),
+          status: highest_severity,
+          url: agent.vuln_url(resource.name, digest)
+        }
+      end
+    end
+    response
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  def quay_get_highest_severity(severities)
+    QUAY_STATUS_TO_COLOUR.keys.each do |s|
+      return s if severities.include? s
+    end
   end
 end
